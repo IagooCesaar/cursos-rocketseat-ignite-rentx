af20c51b84e1ad32b5dd2924d8f791b0af715e02
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-03-17 10:45:09 -0300

    Reading CSV file as Stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,18 @@
+import csvParte from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+    const parseFile = csvParte({
+      delimiter: ",",
+    });
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
