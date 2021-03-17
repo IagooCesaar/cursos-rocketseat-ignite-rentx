acb877f686e08e467940aba604c8f26fc84c412f
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-03-17 14:23:17 -0300

    Initiate Swagger route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(3333);
