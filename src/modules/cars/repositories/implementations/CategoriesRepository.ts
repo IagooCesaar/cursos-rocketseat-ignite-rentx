650a32729caca4459d8d660bbb6cb1e60de3d30f
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-03-17 07:16:11 -0300

    implementations folder for repositories

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,8 +1,8 @@
-import { Category } from "../models/Category";
+import { Category } from "../../models/Category";
 import {
   ICategoriesRepository,
   ICreateCategoryDTO,
-} from "./ICategoriesRepository";
+} from "../ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
