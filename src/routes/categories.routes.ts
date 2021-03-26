f57308e5d44ef0d00228400aee6b13e267db5c90
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-03-26 15:11:32 -0300

    Refactoring categories routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import multer from "multer";
 
-import { createCategoryController } from "../modules/cars/useCases/createCategory";
-import { importCategoryController } from "../modules/cars/useCases/importCategory";
-import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import createCategoryController from "../modules/cars/useCases/createCategory";
+import importCategoryController from "../modules/cars/useCases/importCategory";
+import listCategoriesController from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
 
@@ -12,15 +12,15 @@ const upload = multer({
 });
 
 categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController.handle(request, response);
+  return createCategoryController().handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  return listCategoriesController.handle(request, response);
+  return listCategoriesController().handle(request, response);
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-  return importCategoryController.handle(request, response);
+  return importCategoryController().handle(request, response);
 });
 
 export { categoriesRoutes };
