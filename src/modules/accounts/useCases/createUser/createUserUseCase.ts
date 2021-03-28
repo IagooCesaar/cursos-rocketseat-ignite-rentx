c44b2ea678e081b6ee3055adc88c7190da7f00cc
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-03-28 20:16:33 -0300

    hashing users passwords

--- src/modules/accounts/useCases/createUser/createUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/createUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -16,10 +17,12 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const passwordHash = await hash(password, 10);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
