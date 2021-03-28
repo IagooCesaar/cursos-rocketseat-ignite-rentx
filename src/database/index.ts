0ad299d5b3368dd7932d86494c36e901041b0830
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-03-28 10:21:01 -0300

    add tsyringe and improvment docker config database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,13 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then((options) => {
+  const newOptions = options as IOptions;
+  newOptions.host = "database_ignite";
+  createConnection({
+    ...options,
+  });
+});
