ce52fd5965bab09c2eb890442a74906a90bd07c4
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-03-28 21:36:30 -0300

    add json web token

--- src/modules/accounts/useCases/createUser/createUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/createUserUseCase.ts
@@ -20,7 +20,7 @@ class CreateUserUseCase {
     const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new Error("User alredy exists");
+      throw new Error("User already exists");
     }
 
     const passwordHash = await hash(password, 10);
