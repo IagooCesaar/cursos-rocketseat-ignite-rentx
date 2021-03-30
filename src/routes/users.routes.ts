01650748f9c3121d2fc49a974e655810bb89a026
Iago César <iagocesar.sgs@gmail.com>
2021-03-30 10:23:10 -0300

    Add patch route to change user's avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,24 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/createUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/updateUserAvatarController";
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./temp/avatar"));
+
 const createUserController = new CreateUserController();
 
+const updateUserAvatarController = new UpdateUserAvatarController();
+
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.patch(
+  "/avatar",
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
