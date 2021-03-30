a7596d1127f63d4f63ec47f647bffaedea31b269
Iago César <iagocesar.sgs@gmail.com>
2021-03-30 10:33:54 -0300

    Add ensureAuthenticated middleware

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import multer from "multer";
 
 import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/createUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/updateUserAvatarController";
 
 const usersRoutes = Router();
 
-const uploadAvatar = multer(uploadConfig.upload("./temp/avatar"));
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
 
@@ -17,6 +18,7 @@ usersRoutes.post("/", createUserController.handle);
 
 usersRoutes.patch(
   "/avatar",
+  ensureAuthenticated,
   uploadAvatar.single("avatar"),
   updateUserAvatarController.handle
 );
