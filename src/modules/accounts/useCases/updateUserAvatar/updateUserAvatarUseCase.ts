2cf082e76ef19883fbc8b1aedce82ed8c763cb6e
Iago César <iagocesar.sgs@gmail.com>
2021-03-30 09:47:18 -0300

    Add updateUserAvatarUseCase

---
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
@@ -0,0 +1,24 @@
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+  async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id);
+    user.avatar = avatar_file;
+
+    await this.usersRepository.create(user);
+  }
+}
+
+export { UpdateUserAvatarUseCase };
