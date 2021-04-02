20ba91b09ec88076524a5babf16bd01c07ea4f74
Iago César <iagocesar.sgs@gmail.com>
2021-04-02 14:17:40 -0300

    refact. handlingErrors

--- src/middlewares/handlingErrors.ts
+++ src/middlewares/handlingErrors.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-import { AppError } from "../errors/appError";
+import { AppError } from "@errors/appError";
 
 export function handlingErrors(
   err: Error,
