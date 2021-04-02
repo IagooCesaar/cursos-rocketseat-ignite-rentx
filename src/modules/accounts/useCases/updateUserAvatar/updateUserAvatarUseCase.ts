1bc0b864cd4037ed4c33bc9165d6dfa05fda85ea
Iago César <iagocesar.sgs@gmail.com>
2021-04-02 14:07:18 -0300

    reacft updateUserAvatarUseCase

--- src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
-import { deleteFile } from "../../../../utils/file";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { deleteFile } from "@utils/file";
 
 interface IRequest {
   user_id: string;
