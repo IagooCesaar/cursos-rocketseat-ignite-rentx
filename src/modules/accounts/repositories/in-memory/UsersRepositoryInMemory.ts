a91aab94f1e3011cd94369ef0b6f2f87db50f3bb
Iago César <iagocesar.sgs@gmail.com>
2021-04-02 13:24:31 -0300

    create UsersRepositoryInMemory

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,36 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create({
+    driver_license,
+    email,
+    name,
+    password,
+    avatar,
+  }: ICreateUserDTO): Promise<void> {
+    const user = new User();
+    Object.assign(user, {
+      driver_license,
+      email,
+      name,
+      password,
+      avatar,
+    });
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find((user) => user.email === email);
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+}
+
+export { UsersRepositoryInMemory };
