d9ca18ad5940ea5519bbf54c9429dec5ae0be22e
Iago César <iagocesar.sgs@gmail.com>
2021-04-02 12:34:51 -0300

    create CategoriesRepositoryInMemory.ts

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,30 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories;
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepositoryInMemory };
