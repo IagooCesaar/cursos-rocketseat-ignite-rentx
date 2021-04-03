a7c3357e912e1cc77a35aae46a94ae63b31b4012
Iago César <iagocesar.sgs@gmail.com>
2021-04-03 15:21:07 -0300

    typeorm decorators for Cars entity

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,24 +1,58 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+import { Category } from "./Category";
+
+@Entity("cars")
 class Car {
+  @PrimaryColumn()
   id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column()
   daily_rate: number;
+
+  @Column()
   available: boolean;
+
+  @Column()
   license_plate: string;
+
+  @Column()
   fine_amount: number;
+
+  @Column()
   brand: string;
+
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({
+    name: "category_id",
+  })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
-  updated_at: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
       this.available = true;
-      this.created_at = new Date();
     }
   }
 }
