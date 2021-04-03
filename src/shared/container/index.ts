84c311b79ba96f1b7217ffe7cddfc5bc5f9027fb
Iago César <iagocesar.sgs@gmail.com>
2021-04-03 15:23:38 -0300

    register singleton form CarsRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,10 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
@@ -21,3 +23,5 @@ container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
 );
+
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
