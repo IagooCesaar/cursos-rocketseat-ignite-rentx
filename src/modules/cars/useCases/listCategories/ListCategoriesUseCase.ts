02876c55b07eec69455c83e140288a4e9688519f
Iago César <iagocesar.sgs@gmail.com>
2021-04-07 20:13:12 -0300

    Fix import of Category Entity

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
-import { Category } from "@modules/cars/entities/Category";
+import { Category } from "@modules/cars/infra/typeorm/entities/Category";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 
 @injectable()
