b99cdae81d33e66c87a4fb366e7f167b8616eb45
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-08 17:12:25 -0300

    fix lint

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,8 +2,12 @@ import crypto from "crypto";
 import multer from "multer";
 import { resolve } from "path";
 
+interface IUploadConfig {
+  storage: multer.StorageEngine;
+}
+
 export default {
-  upload(folder: string) {
+  upload(folder: string): IUploadConfig {
     return {
       storage: multer.diskStorage({
         destination: resolve(__dirname, "..", "..", folder),
--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from "../entities/Category";
+import { Category } from "../infra/typeorm/entities/Category";
 
 interface ICreateCategoryDTO {
   name: string;
