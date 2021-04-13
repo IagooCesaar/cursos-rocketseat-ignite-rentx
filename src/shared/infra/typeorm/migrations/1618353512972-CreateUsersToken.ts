157ba48c2bd3f10e52b7672546e931e73421b6b6
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-13 19:45:21 -0300

    CreateUsersToken migration

---
+++ src/shared/infra/typeorm/migrations/1618353512972-CreateUsersToken.ts
@@ -0,0 +1,35 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsersToken1618353512972 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users_tokens",
+        columns: [
+          { name: "id", type: "uuid", isPrimary: true},
+          { name: "refresh_token", type: "varchar" },
+          { name: "user_id", type: "uuid" },
+          { name: "expires_date", type: "timestamp" },
+          { name: "created_at", type: "timestamp", default: "now()"}
+
+        ],
+        foreignKeys: [
+          {
+            name: "FK_Users_Tokens_User",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE"
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users_tokens")
+  }
+
+}
