4f5bf92582cf95e1a22e2acf7ca931c17a31bca6
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-13 10:18:26 -0300

    changes to recreate container with wsl2

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -8,9 +8,11 @@ interface IUploadConfig {
 
 export default {
   upload(folder: string): IUploadConfig {
+    const destination = resolve(__dirname, "..", "..", folder);
+
     return {
       storage: multer.diskStorage({
-        destination: resolve(__dirname, "..", "..", folder),
+        destination,
         filename: (request, file, callback) => {
           const fileHash = crypto.randomBytes(16).toString("hex");
           const fileName = `${fileHash}-${file.originalname}`;
