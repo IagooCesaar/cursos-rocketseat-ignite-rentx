011dc54a8ae72068888f61d0898030f56b22f52f
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-14 10:47:25 -0300

    change auth config

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,7 @@
 export default {
   secret_token: "d3acee670ad999360c2b5315b8f5b71c",
-  expires_in: "15m",
+  expires_in: "30m",
   secret_refresh_token: "818088b95d6fe366c9bf357d2334f3fb",
-  expires_in_refresh_token: "30d",
+  expires_in_refresh_token: "8h",
   expires_refresh_token_days: 30,
 };
