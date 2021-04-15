b3ac8e6f9c2ed38474f101cc83ad4c584430ed27
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-15 20:44:11 -0300

    implements sendMail

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -4,6 +4,7 @@ import { v4 as uuidV4 } from "uuid";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 
 import { SendForgotPasswordMailError } from "./SendForgotPasswordMailError";
 
@@ -17,7 +18,10 @@ class SendForgotPasswordMailUseCase {
     private UsersTokensRepository: IUsersTokensRepository,
 
     @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+
+    @inject("EtherealMailProvider")
+    private mailProvider: IMailProvider
   ) {}
 
   async execute(email: string): Promise<void> {
@@ -32,6 +36,12 @@ class SendForgotPasswordMailUseCase {
       refresh_token: token,
       user_id: user.id,
       expires_date,
+    });
+
+    this.mailProvider.sendMail({
+      to: user.email,
+      subject: "Recuperação de senha",
+      body: `O link para o reset de sua senha é ${token}`,
     });
   }
 }
