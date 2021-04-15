6de3f3b8e35aaa47fdd209e498143995f664dfe8
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-15 14:42:58 -0300

    UsersTokensRepositoryInMemory

---
+++ src/modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory.ts
@@ -0,0 +1,35 @@
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { UsersTokens } from "@modules/accounts/infra/typeorm/entities/UserTokens";
+
+import { IUsersTokensRepository } from "../IUsersTokensRepository";
+
+class UsersTokensRepositoryInMemory implements IUsersTokensRepository {
+  private usersTokens: UsersTokens[] = [];
+
+  async create(data: ICreateUserTokenDTO): Promise<UsersTokens> {
+    const userToken = new UsersTokens();
+    Object.assign(userToken, data);
+    this.usersTokens.push(userToken);
+    return userToken;
+  }
+
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UsersTokens> {
+    return this.usersTokens.find(
+      (userToken) =>
+        userToken.user_id === user_id &&
+        userToken.refresh_token === refresh_token
+    );
+  }
+
+  async deleteById(id: string): Promise<void> {
+    const userTokenIndex = this.usersTokens.findIndex(
+      (userToken) => userToken.id === id
+    );
+    this.usersTokens.splice(userTokenIndex, 1);
+  }
+}
+
+export { UsersTokensRepositoryInMemory };
