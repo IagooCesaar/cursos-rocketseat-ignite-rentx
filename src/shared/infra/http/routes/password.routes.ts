7cfba256f4b23ad51b1da07544afbadf210665c7
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-15 22:14:49 -0300

    reset password route

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { ResetUsersPasswordController } from "@modules/accounts/useCases/resetUsersPassword/ResetUsersPasswordController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetUsersPasswordContoller = new ResetUsersPasswordController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetUsersPasswordContoller.handle);
 
 export { passwordRoutes };
