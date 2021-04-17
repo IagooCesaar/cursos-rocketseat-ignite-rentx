1a5cfc1f2c700a6c8bf24c1094cd4b5001d6391d
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-17 17:49:32 -0300

    serving coverage report

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response } from "express";
+import path from "path";
 import swaggerUi from "swagger-ui-express";
 
 import "express-async-errors";
@@ -18,6 +19,26 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(
+  "/api-coverage",
+  express.static(
+    path.resolve(__dirname, "..", "..", "..", "..", "coverage", "lcov-report")
+  )
+);
+app.get("/api-coverage", (request: Request, response: Response) => {
+  return response.sendFile(
+    path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "coverage",
+      "lcov-report",
+      "index.html"
+    )
+  );
+});
 app.use(router);
 
 app.use(handlingErrors);
