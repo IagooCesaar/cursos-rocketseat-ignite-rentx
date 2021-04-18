af9582a9542bb349964ca59fa6d1200880c5eab7
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-18 10:20:38 -0300

    expose avatar_url with class-transformer

--- src/modules/accounts/infra/typeorm/entities/User.ts
+++ src/modules/accounts/infra/typeorm/entities/User.ts
@@ -1,3 +1,4 @@
+import { Expose } from "class-transformer";
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
@@ -26,6 +27,18 @@ class User {
 
   @CreateDateColumn()
   created_at: Date;
+
+  @Expose({ name: "avatar_url" })
+  getAvatarUrl(): string {
+    switch (process.env.STORAGE_TYPE) {
+      case "local":
+        return `${process.env.APP_API_URL}/avatar/${this.avatar}`;
+      case "s3":
+        return `${process.env.AWS_BUCKET_URL}/avatar/${this.avatar}`;
+      default:
+        return null;
+    }
+  }
 
   constructor() {
     if (!this.id) {
