8cf7bdac6460b759719668dddc4832f6f6b8869c
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-18 17:27:34 -0300

    use default localhost database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database_ignite"): Promise<Connection> => {
+// host = "database_ignite"
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
