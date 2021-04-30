ccb4e0cb6c5235da1e46d49ac5bffc54aea51257
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-30 09:13:50 -0300

    improvments and remove comments

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -21,25 +21,20 @@ import { router } from "./routes";
 createConnection();
 const app = express();
 
+app.use(rateLimiter);
+
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   integrations: [
-    // enable HTTP calls tracing
     new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
     new Tracing.Integrations.Express({ app }),
   ],
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 });
-
-app.use(rateLimiter);
-app.use(express.json());
-
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
+
+app.use(express.json());
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
@@ -69,16 +64,7 @@ app.get("/api-coverage", (request: Request, response: Response) => {
 app.use(cors());
 app.use(router);
 
-app.use(
-  Sentry.Handlers.errorHandler({
-    shouldHandleError(error) {
-      if (error.status === 429 || error.status === 500) {
-        return true;
-      }
-      return false;
-    },
-  })
-);
+app.use(Sentry.Handlers.errorHandler());
 app.use(handlingErrors);
 
 export { app };
