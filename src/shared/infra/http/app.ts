89fedf844427b7f92cfe94eb3fe09a80a6a58321
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-30 09:02:33 -0300

    add Sentry for tracking errors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -9,6 +9,8 @@ import path from "path";
 import swaggerUi from "swagger-ui-express";
 
 import upload from "@config/upload";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import { handlingErrors } from "@shared/infra/http/middlewares/handlingErrors";
 import createConnection from "@shared/infra/typeorm";
 
@@ -17,11 +19,27 @@ import rateLimiter from "./middlewares/rateLimiter";
 import { router } from "./routes";
 
 createConnection();
-
 const app = express();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 app.use(rateLimiter);
 app.use(express.json());
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
@@ -50,6 +68,17 @@ app.get("/api-coverage", (request: Request, response: Response) => {
 
 app.use(cors());
 app.use(router);
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      if (error.status === 429 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
 app.use(handlingErrors);
 
 export { app };
